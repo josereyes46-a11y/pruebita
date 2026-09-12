b7a507882e186fb3da1b7bcb7abaa90acb7f1c9c
José Alfredo Reyes Cabrera <jose.reyes46@uabc.edu.mx>
2026-09-11 17:19:07 -0700

    Inicializar proyecto de pruebas

---
+++ test_calculadora.py
@@ -0,0 +1,9 @@
+from calculadora import Calculadora
+
+def test_add():
+        calc = Calculadora() 
+        assert calc.add(2, 3) == 5
+
+def test_quit():
+        calc = Calculadora()
+        assert calc.quit(4, 3) == 1
