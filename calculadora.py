3ca6182d17028555e63271e666c9b1689d88028e
José Alfredo Reyes Cabrera <jose.reyes46@uabc.edu.mx>
2026-09-11 17:34:34 -0700

    Se agrego el metodo de resta

--- calculadora.py
+++ calculadora.py
@@ -2,3 +2,6 @@ class Calculadora:
     
     def add(self, a, b): 
         return a + b
+
+    def quit(self, c, d):
+        return c - d
--- test_calculadora.py
+++ test_calculadora.py
@@ -5,5 +5,5 @@ def test_add():
         assert calc.add(2, 3) == 5
 
 def test_quit():
-        calc = Calculadora()
-        assert calc.quit(4, 3) == 1
+        calc = Calculadora() 
+        assert calc.quit(10, 7) == 3
